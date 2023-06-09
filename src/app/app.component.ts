f9b634a3a54a2e20db0999b8e2ac747296db1050
Orel_tm71 <Orel_tm71@student.itstep.org>
2023-06-09 20:48:53 +0300

    Converter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,37 +9,74 @@ import { CurrencyapidataServiceService } from './currencyapidata-service.service
 export class AppComponent {
   title = 'prog';
   currjson: any=[];
-  base='USD';
-  cont2='USD';
-  result: string='1';
-  changebase(a:string){
-    this.base = a;
-    console.log(this.base);
+  left='USD'; //default value of left select
+  right='USD'; //default value of right select
+  result: string='1'; //convert's result 
+  inp1='1'; //value input actual
+  inpId='1'; //reactive variable for input1
+  inpId2='1'; //reactive variable for input2
+  ident="left"; //shows what select was changed
+
+  mainFunc(valueSelectLeft:string, valueInput: string, valueSelectRight: string, id: string){
+    this.left = valueSelectLeft;
+    this.inp1=valueInput;
+    this.right=valueSelectRight;
+    this.ident=id;
+    
+   this.balance2(valueInput);
+   this.convert();
   }
 
-  tocountry(b:string){
-    this.cont2=b;
-    console.log(this.cont2);
+  tocountry(b:string, id: string){ //function for change currency base
+    if(this.ident===id){
+      this.left=b;   
+    }
+    else{
+      this.right=b;      
+    }
+    
+    this.convert();
   }
 
   constructor(private currency: CurrencyapidataServiceService){}
 
+  balance()
+  {
+    if(this.ident==='left'){
+      this.inpId2=this.result;  
+    }
+    else if(this.ident==='right'){
+      this.inpId=this.result;
+    }   
+  }
+  balance2(q: string)
+  {
+    if(this.ident==='left'){
+      this.inpId=q;  
+    }
+    else if(this.ident==='right'){
+      this.inpId2=q;
+    }   
+  }
   convert(){
-    this.currency.getCurrencyData(this.base).subscribe(data =>{
+    this.currency.getCurrencyData(this.left).subscribe(data =>{
       this.currjson=JSON.stringify(data);
       this.currjson=JSON.parse(this.currjson);
-      switch (this.cont2) {
+      switch (this.right) {
         case "USD":
-          this.result=this.currjson.rates.USD;
+          this.result=(this.currjson.rates.USD*(+this.inp1)).toString();
+          this.balance();        
           break; 
           case "UAH":
-          this.result=this.currjson.rates.UAH;
+          this.result=(this.currjson.rates.UAH*(+this.inp1)).toString();
+          this.balance();
           break;
           case "EUR":
-          this.result=this.currjson.rates.EUR;
+          this.result=(this.currjson.rates.EUR*(+this.inp1)).toString();
+          this.balance();
           break;
           default:
-            break;
+          break;
       }
     })
   }
